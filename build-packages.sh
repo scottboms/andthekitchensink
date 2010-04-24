662d15baa64fc746887ae43c63c26521070c7c23
Scott Boms <scottboms@gmail.com>
2010-04-24 02:48:44 +0000

    Updated MySQL build to version 5.1.46 release.

--- build-packages.sh
+++ build-packages.sh
@@ -386,8 +386,7 @@ echo "##------ Building mysql"
 PKG="${DIR}/mysql-5*/"
 if [ -d ${PKG} ]; then
   cd ${DIR}/mysql-5*
-  CC=gcc CFLAGS="-O3 -fno-omit-frame-pointer" CXX=gcc CXXFLAGS="-O3 -fno-omit-frame-pointer -felide-constructors -fno-exceptions -fno-rtti"
-  ./configure --prefix=${PREFIX}/mysql --with-extra-charsets=complex --enable-thread-safe-client --enable-local-infile --enable-shared --with-zlib-dir=bundled --with-big-tables --with-readline --with-plugins=all --without-docs
+  CC=gcc CFLAGS="-O3 -fno-omit-frame-pointer" CXX=gcc CXXFLAGS="-O3 -fno-omit-frame-pointer -felide-constructors -fno-exceptions -fno-rtti" ./configure --prefix=${PREFIX}/mysql --with-extra-charsets=complex --enable-thread-safe-client --enable-local-infile --enable-shared --with-zlib-dir=bundled --with-big-tables --with-readline --with-plugins=all --without-docs
   make
   make install
   clear
