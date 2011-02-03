0df3d17cb6874f21f90f8a6d03e93f0635118bd3
Scott Boms <scottboms@gmail.com>
2011-02-03 02:32:42 +0000

    Updated git to version 1.7.4 release and added support for installing dodocumentation which really should have been there before.

--- build-packages.sh
+++ build-packages.sh
@@ -180,7 +180,7 @@ if [ -d ${PKG} ]; then
   cd ${DIR}/git-*
   ./configure --prefix=${PREFIX}
   make all
-  make install
+  make install install-doc install-html
   clear
 else
   echo "There was a problem with git"
