7c3607c64dbabca3cc4cd4323d323fc40abae796
Scott Boms <scottboms@gmail.com>
2013-12-25 08:14:48 +0000

    Fixing standalone MySQL 2 gem build instructions for Ruby 2.0 and others bits and pieces.

--- pkg/mysql2gem.sh
+++ pkg/mysql2gem.sh
@@ -3,6 +3,6 @@
 #------------------------------------------------------------------------------
 echo "##------ Building MySQL2 for Ruby Gem"
 
-sudo env ARCHFLAGS="-arch x86_64" gem install mysql2 -- --with-mysql-dir=${PREFIX}/mysql
+sudo env ARCHFLAGS="-arch x86_64" gem install mysql2 -- --with-mysql-config=${PREFIX}/mysql/bin/mysql_config
 
-sudo install_name_tool -change libmysqlclient.18.dylib ${PREFIX}/mysql/lib/libmysqlclient.18.dylib ${PREFIX}/lib/ruby/gems/2.0.0/gems/mysql2-0.3.13/lib/mysql2/mysql2.bundle
+sudo install_name_tool -change libmysqlclient.18.dylib ${PREFIX}/mysql/lib/libmysqlclient.18.dylib ${PREFIX}/lib/ruby/gems/2.0.0/gems/mysql2-0.3.14/lib/mysql2/mysql2.bundle
