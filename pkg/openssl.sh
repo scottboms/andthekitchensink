5a7a691d01b27c38afcc43f60de2d5b0ebc3c91b
Scott Boms <scottboms@gmail.com>
2014-08-05 06:31:31 +0000

    A few package updates.

--- pkg/openssl.sh
+++ pkg/openssl.sh
@@ -3,13 +3,23 @@
 #------------------------------------------------------------------------------
 echo "##------ Building OpenSSL"
 PKG="${DIR}/openssl-*/"
+VERS="1.0.1h"
+SYMLINK="${PREFIX}/openssl"
 if [ -d ${PKG} ]; then
   cd ${DIR}/openssl-*
-  ./config --prefix=${PREFIX}
+  ./configure darwin64-x86_64-cc --prefix=${PREFIX}/openssl-${VERS}
   make
   make test
   make install
   clear
+
+  if[ -f ${SYMLINK} ]; then
+    ln -s ${PREFIX}/openssl-${VERS} ${PREFIX}/openssl
+  else
+    rm ${PREFIX}/openssl-${VERS}
+    ln -s ${PREFIX}/openssl-${VERS} ${PREFIX}/openssl
+  fi
+
 else
   echo "There was a problem with OpenSSL"
 fi
